de558abedc10ee52755c1727ad77a79b290dca2a
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2018-01-09 21:02:22 -0500

    add bodyparser

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 5000
 const app = express()
-const bodyParser = require('bodyParser')
+const bodyParser = require('bodyparser')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
