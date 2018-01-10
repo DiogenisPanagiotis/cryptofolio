ce3f24a4541a89f9f200197c0faab99d7a1e3a8c
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2018-01-09 20:47:41 -0500

    fix dependencies

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
-import promise from 'redux-promise-middleware';
+import promise from 'redux-promise-middleware'
 import rootReducer from './reducers/rootReducer'
 
 const createStoreWithMiddleware = applyMiddleware(
