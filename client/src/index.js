38e671fc92eb6fdc19617018fb076494e85d7e95
Diogenis Panagiotis <diogenis.panagiotis@gmail.com>
2018-01-12 21:24:05 -0500

    handle other routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import registerServiceWorker from './registerServiceWorker'
@@ -14,6 +14,7 @@ render(
         	<div>
 	            <Route exact path="/" component={App}/>
 	            <Route exact path="/dashboard" component={dashboardContainer}/>
+	            <Redirect from="*" to="/" />
         	</div>
         </BrowserRouter>
     </Provider>,
--- server/api/routes.js
+++ server/api/routes.js
@@ -23,7 +23,7 @@ module.exports = function (app, express) {
 
     app.use('/api', router)
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'))
     })
 
